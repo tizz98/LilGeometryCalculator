b5c0d7c97397b32f1f34731c8265e1931a12ae4c
tizz98 <dev.tizz98@gmail.com>
2016-02-19 14:18:34 -0800

    clear as mud conversions

--- LilGeometryCalculator/frmCalc.vb
+++ LilGeometryCalculator/frmCalc.vb
@@ -11,7 +11,15 @@
     End Sub
 
     Private Sub rdoConvertUS_CheckedChanged(sender As Object, e As EventArgs) Handles rdoConvertUS.CheckedChanged
-        ' todo
+        If rdoConvertUS.Checked Then
+            txtAnswer.Text = currentFormula.convertMetricToUs(Convert.ToDouble(txtAnswer.Text))
+        End If
+    End Sub
+
+    Private Sub rdoMetric_CheckedChanged(sender As Object, e As EventArgs) Handles rdoMetric.CheckedChanged
+        If rdoMetric.Checked Then
+            txtAnswer.Text = currentFormula.convertUsToMetric(Convert.ToDouble(txtAnswer.Text))
+        End If
     End Sub
 
     Private Sub resetForm()
@@ -59,6 +67,10 @@
     Private Sub lstFormula_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstFormula.SelectedIndexChanged
         resetInput()
 
+        If lstFormula.SelectedIndex = -1 Then
+            Return
+        End If
+
         currentFormula = (From frmla In currentShape.formulas Where frmla.name = lstFormula.SelectedItem Select frmla).First
 
         ' setup form for action!
@@ -77,6 +89,19 @@
             lblBottomInput.Show()
             txtBottomInput.Show()
         End If
+
+        rdoConvertUS.Text = currentFormula.getUsUnitName()
+        rdoMetric.Text = currentFormula.getMetricUnitName()
+    End Sub
+
+    Private Sub btnClearFormula_Click(sender As Object, e As EventArgs) Handles btnClearFormula.Click
+        resetInput()
+        currentFormula = Nothing
+        lstFormula.SelectedIndex = -1
+    End Sub
+
+    Private Sub btnClearValue_Click(sender As Object, e As EventArgs) Handles btnClearValue.Click
+        resetInputValues()
 
         rdoConvertUS.Text = currentFormula.getUsUnitName()
         rdoMetric.Text = currentFormula.getMetricUnitName()
